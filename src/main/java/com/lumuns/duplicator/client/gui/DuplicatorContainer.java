fd4f4f8e785f5ab287edc70798e3ea496883bdeb
Michael Hillcox <hello.michaelhillcox@gmail.com>
2018-11-05 21:30:51 +0000

    Progess on gui

--- src/main/java/com/lumuns/duplicator/client/gui/DuplicatorContainer.java
+++ src/main/java/com/lumuns/duplicator/client/gui/DuplicatorContainer.java
@@ -4,6 +4,7 @@ import com.lumuns.duplicator.common.tileentity.TileEntityDuplicator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -13,6 +14,22 @@ public class DuplicatorContainer extends Container {
 
     public DuplicatorContainer(InventoryPlayer player, TileEntityDuplicator tileEntity) {
         duplicator = tileEntity;
+
+        // Slots for the main inventory
+        for (int row = 0; row < 3; ++row) {
+            for (int col = 0; col < 9; ++col) {
+                int x = 8 + col * 18;
+                int y = row * 18 + 84;
+                this.addSlotToContainer(new Slot(player, col + row * 9 + 9, x, y));
+            }
+        }
+
+        // Slots for the hotbar
+        for (int row = 0; row < 9; ++row) {
+            int x = 8 + row * 18;
+            int y = 58 + 84;
+            this.addSlotToContainer(new Slot(player, row, x, y));
+        }
     }
 
     @Override
--- src/main/java/com/lumuns/duplicator/common/network/GuiHandler.java
+++ src/main/java/com/lumuns/duplicator/common/network/GuiHandler.java
@@ -1,7 +1,9 @@
 package com.lumuns.duplicator.common.network;
 
 import com.lumuns.duplicator.client.gui.DuplicatorContainer;
+import com.lumuns.duplicator.common.tileentity.TileEntityDuplicator;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
@@ -11,12 +13,12 @@ public class GuiHandler implements IGuiHandler {
     @Nullable
     @Override
     public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-        return new DuplicatorContainer();
+        return new DuplicatorContainer(player.inventory, (TileEntityDuplicator) world.getTileEntity(new BlockPos(x, y, z)));
     }
 
     @Nullable
     @Override
     public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-        return null;
+        return new DuplicatorContainer(player.inventory, (TileEntityDuplicator) world.getTileEntity(new BlockPos(x, y, z)));
     }
 }
