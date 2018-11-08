e01029dfffbb6ab71c37e2e0d64107d5c693741a
Michael Hillcox <hello.michaelhillcox@gmail.com>
2018-11-08 22:17:02 +0000

    Fixed register issues

--- src/main/java/com/lumuns/duplicator/common/network/RegisterEventHandler.java
+++ src/main/java/com/lumuns/duplicator/common/network/RegisterEventHandler.java
@@ -1,13 +1,15 @@
 package com.lumuns.duplicator.common.network;
 
-import com.lumuns.duplicator.common.blocks.BlockDuplicator;
 import com.lumuns.duplicator.common.blocks.ModBlocks;
 import com.lumuns.duplicator.common.tileentity.TileEntityDuplicator;
 import com.lumuns.duplicator.common.utils.Ref;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -25,8 +27,17 @@ public class RegisterEventHandler {
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        for(Block block: ModBlocks.BLOCKS)
+        for(Block block: ModBlocks.BLOCKS) {
             event.getRegistry().register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+        }
+    }
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event)
+    {
+        for (Block block: ModBlocks.BLOCKS) {
+            ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
+        }
     }
 
 }
--- src/main/java/com/lumuns/duplicator/common/proxy/ClientProxy.java
+++ src/main/java/com/lumuns/duplicator/common/proxy/ClientProxy.java
@@ -1,10 +1,5 @@
 package com.lumuns.duplicator.common.proxy;
 
-import com.lumuns.duplicator.common.blocks.ModBlocks;
-import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -15,11 +10,6 @@ public class ClientProxy {
     }
 
     public void preInit(FMLPreInitializationEvent event) {
-        for (Block block: ModBlocks.BLOCKS)
-            ModelLoader.setCustomModelResourceLocation(
-                    Item.getItemFromBlock(block),
-                    0,
-                    new ModelResourceLocation(block.getRegistryName(), "inventory"));
     }
 
     public void postInit(FMLPostInitializationEvent event) {
